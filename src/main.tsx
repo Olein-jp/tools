b07d015f9e3e2e437105c20970b36deb440d261d
Koji Kuno <koji.kuno@gmail.com>
2026-02-25 10:30:54 +0900

    routerをBrowserRouterへ変更してURLの#を除去

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter
+    <BrowserRouter
       future={{
         v7_startTransition: true,
         v7_relativeSplatPath: true
       }}
     >
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
